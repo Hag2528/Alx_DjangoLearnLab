77441e3230b51ef937723d6f4b046a539369a99b
Hagos Chekole <Hagochek06@gmail.com>
2024-08-11 12:06:45 +0300

    admin py file is commited

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book  # Import the Book model
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Customize list view
+    list_filter = ('author', 'publication_year')  # Add list filters
+    search_fields = ('title', 'author')  # Enable searching
+
+admin.site.register(Book, BookAdmin)
