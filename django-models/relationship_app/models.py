3688fb44c9bcb2eb2c17c0e0b563883bc1827c66
Hagos Chekole <Hagochek06@gmail.com>
2024-08-17 22:38:39 +0300

    models are modified

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,17 +1,3 @@
-# from django.db import models
-
-# # Create your models here.
-# class Author(models.Model):
-#     name=models.CharField()
-# class Book(models.Model):
-#     title=models.CharField()
-#     Author=models.ForeignKey(Author,on_delete=models.CASCADE)
-# class Library(models.Model):
-#     name=models.CharField()
-#     books=models.ManyToManyField(Book, related_name='books')
-# class Librarian(models.Model):
-#     name=models.CharField()
-#     Library=models.OneToOneField(Library,on_delete=models.CASCADE)
 from django.db import models
 class Author(models.Model):
   name = models.CharField(max_length=255)
