86b02de89b825f88553b41eab1bb1933de07b382
Hagos Chekole <Hagochek06@gmail.com>
2024-08-18 00:04:26 +0300

    template, view and urls are done

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render , redirect
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.forms import UserCreationForm
-
-def register(request):
-     form=UserCreationForm(request.Post)
-     if form.is_valid():
-          form.save()
-          return redirect("login")
-
-
 # Create your views here.
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Library,Book 
+
+def list_books(request):
+    books = Book.objects.all() 
+    book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return render(request, 'list_books.html', {'books': book_list})  # Render with context
+
+
+class LibraryDetailView(DetailView):
+    model = Library  # Specify model for DetailView
+    template_name = 'library_detail.html'  # Template for rendering
+
+def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['books'] = self.object.books.all()  # Get books related to the library
+    return context
