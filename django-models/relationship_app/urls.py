2949199bb8020e3815df2a53a88da391119c7194
Hagos Chekole <Hagochek06@gmail.com>
2024-08-18 00:46:24 +0300

    commited is done

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from . import views
+from .views import Author, Book , Library, Librarian
+from django.contrib.auth.views import LogoutView
+from.import views
 
 urlpatterns = [
+    path("Author",Author.as_view(),name="register"),
+    path("Book",Book.as_view(),name="Book"),
+    path("Librarian",Librarian.as_view(),name="Librarian"),
     path('books/', views.list_books, name='list_books'),
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
