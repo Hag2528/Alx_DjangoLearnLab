ff7517502095987b0f1b2f5b1f770178546ab293
Hagos Chekole <Hagochek06@gmail.com>
2024-08-18 02:20:18 +0300

    url commited

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('books/', views.list_books, name='book_list'),
     path('libraries/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+
