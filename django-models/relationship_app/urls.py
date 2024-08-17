85a01f6a39513c31dbfebfe60387d1afb6edb7a4
Hagos Chekole <Hagochek06@gmail.com>
2024-08-18 02:34:53 +0300

    urls is done

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
+    path('register',views.register.as_view(), name='register'),
     path('logout/', views.logout, name='logout'),
     path('books/', views.list_books, name='book_list'),
     path('libraries/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
-
-
