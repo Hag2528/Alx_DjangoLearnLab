8ba0b2a154b6332f6865de03f3493800b50f3561
Hagos Chekole <Hagochek06@gmail.com>
2024-08-17 22:55:19 +0300

    relationship_app are commited

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,7 +1,7 @@
 from relationship_app.models import Author, Book, Library, Librarian
 def get_books_by_author(author_name):
    Author=Author.objects.get(name=author_name)
-   return Author.objects.filter(author=author)
+   return Author.objects.filter(author=Author)
 
 def get_books_in_library(library_name):
   library = Library.objects.get(name=library_name)
