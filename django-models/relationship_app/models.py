25ffff65dd61bb68f0bde48f26faf336830166df
Hagos Chekole <Hagochek06@gmail.com>
2024-08-18 11:13:10 +0300

    django model is modified

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,29 @@
 from django.db import models
+
+from django.contrib.auth.models import User
+from django.db import models
+
+class Role(models.TextChoices):
+    ADMIN = 'ADMIN', 'Admin'
+    LIBRARIAN = 'LIBRARIAN', 'Librarian'
+    MEMBER = 'MEMBER', 'Member'
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    role = models.CharField(max_length=10, choices=Role.choices,   
+ default=Role.MEMBER)
+
+    def __str__(self):
+        return f"{self.user.username} ({self.get_role_display()})"
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+# post_save.connect(create_user_profile, sender=User) 
+
+
+
+
 class Author(models.Model):
   name = models.CharField(max_length=255)
 
