4edd4b261952ea7087d9721a35f7d714d844b79f
Hagos Chekole <Hagochek06@gmail.com>
2024-08-18 03:30:56 +0300

    urls are commited

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import list_books
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
-    path('register',views.register.as_view("LogoutView.as_view(template_name=", "LoginView.as_view(template_name="),
+    path('register/',views.register.as_view("LogoutView.as_view(template_name=", "LoginView.as_view(template_name=")),
     path('logout/', views.logout, name='logout'),
     path('books/', views.list_books, name='book_list'),
     path('libraries/', views.LibraryDetailView.as_view(), name='library_detail'),
