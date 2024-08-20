f8ea20c2f2a2307ae84a082ce6641c5d79093244
Hagos Chekole <Hagochek06@gmail.com>
2024-08-20 13:53:44 +0300

    models are committed

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -75,19 +75,44 @@ def create_user_profile(sender, instance, created, **kwargs):
 #         UserProfile.objects.create(user=instance)
 # signals.post_save.connect(create_user_profile, sender= User) 
 
+# class Author(models.Model):
+#   name = models.CharField(max_length=100)
+#    return self.name
+# class Book(models.Model):
+#  title = models.CharField()
+#  author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')   
+
+# class Library(models.Model):
+#   name = models.CharField(max_length=100)   
+#   books = models.ManyToManyField(Book,related_name='libraries')
+
+
+# class Librarian(models.Model):
+#   name = models.CharField(max_length=100)
+#   library = models.OneToOneField(Library, on_delete=models.CASCADE , related_name='libraries')
+
 class Author(models.Model):
-  name = models.CharField(max_length=100)
+  name = models.CharField(max_length=255)
+
+  def __str__(self):
+    return self.name
 
 class Book(models.Model):
- title = models.CharField()
- author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')   
+ title = models.CharField(max_length=255)
+ author = models.ForeignKey(Author, on_delete=models.CASCADE)
+ def __str__(self):
+    return self.title
 
 class Library(models.Model):
-  name = models.CharField(max_length=100)   
-  books = models.ManyToManyField(Book,related_name='libraries')
+  name = models.CharField(max_length=255)   
 
+
+  def __str__(self):
+    return self.name
 
 class Librarian(models.Model):
-  name = models.CharField(max_length=100)
-  library = models.OneToOneField(Library, on_delete=models.CASCADE , related_name='libraries')
+  name = models.CharField(max_length=255)
+  library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
+  def __str__(self):
+    return self.name
