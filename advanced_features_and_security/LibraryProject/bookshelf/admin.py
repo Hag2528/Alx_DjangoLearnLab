052b9ce8bc7502f99e2044f5e4262f8f2d1a84db
Hagos Chekole <Hagochek06@gmail.com>
2024-08-24 22:31:37 +0300

    bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
 
 # Register your models here.
 from .models import Book  # Import the Book model
@@ -9,3 +11,18 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')  # Enable searching
 
 admin.site.register(Book, BookAdmin)
+
+class CustomUserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Personal Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'date_of_birth', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
