4653c509745f937ecce015fff685941945807b38
Hagos Chekole <Hagochek06@gmail.com>
2024-08-24 23:08:51 +0300

    users views is done

--- advanced_features_and_security/users/views.py
+++ advanced_features_and_security/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import permission_required
 
-# Create your views here.
+
+@permission_required('app_name.view_mymodel', raise_exception=True)
+def view_mymodel(request, pk):
+    # Fetch MyModel instance
+    mymodel = MyModel.objects.get(pk=pk)
+    # ... (process and display MyModel data)
+    return render(request, 'view_mymodel.html', {'mymodel': mymodel})
+
+@permission_required('app_name.create_mymodel', raise_exception=True)
+def create_mymodel(request):
+    if request.method == 'POST':
+        # ... (validate and save new MyModel instance)
+        return redirect('success_url')  # Replace with appropriate redirect URL
+    # ... (display form for creating MyModel)
+    return render(request, 'create_mymodel.html')
