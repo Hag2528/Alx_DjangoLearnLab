ddbcd1691a5ec8d2be21498d8373e9531f05af93
Hag2528 <hagochek53@gmail.com>
2024-08-26 00:08:02 +0300

    Update settings.py

--- advanced_features_and_security/advanced_features_and_security/settings.py
+++ advanced_features_and_security/advanced_features_and_security/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ycew5_3cc25d1b^9xyayt6=(9tsv$0t6+4p@xmrv1^wz#q2mqn'
 
