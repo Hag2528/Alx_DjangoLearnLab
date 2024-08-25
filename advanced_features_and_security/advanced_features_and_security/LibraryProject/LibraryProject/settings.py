ab92d6ad421a87aae6ec15b46b91748945a116d0
Hagos Chekole <Hagochek06@gmail.com>
2024-08-25 12:48:16 +0300

    library Project are committed

--- advanced_features_and_security/advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)x@j1^113a^!ui%ehu=ug86x!u$coinpbea@2zmu%7g5ka92&w'
 SECURE_SSL_REDIRECT = True
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
