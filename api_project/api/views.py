0c2a2a3f7dc57eebd6944948ef4bf9496a183d97
Hagos Chekole <Hagochek06@gmail.com>
2024-08-31 16:44:12 +0300

    api project has been committed

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Book  # Assuming Book model is in the same app
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookList(ListAPIView):
+  queryset = Book.objects.all()  # Get all books
+  serializer_class = BookSerializer
