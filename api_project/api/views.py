1dbf9a99bb5390162072f00f88eee16cc7d57777
Hagos Chekole <Hagochek06@gmail.com>
2024-08-31 16:48:55 +0300

    api view has been committed

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework import generics 
 from .models import Book  # Assuming Book model is in the same app
 from .serializers import BookSerializer
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
   queryset = Book.objects.all()  # Get all books
   serializer_class = BookSerializer
