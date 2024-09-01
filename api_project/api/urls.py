588d4fbfa67b0022eabd099b63cc17b19248475a
Hagos Chekole <Hagochek06@gmail.com>
2024-09-01 08:52:27 +0300

    api project has been done

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -5,3 +5,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/',BookList.as_view()),
 ]
+
+
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet  # Assuming BookViewSet is in views.py
+
+router = DefaultRouter()
+router.register(prefix='books', viewset=BookViewSet)
+
+urlpatterns = router.urls
+
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,3 +6,17 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
   queryset = Book.objects.all()  # Get all books
   serializer_class = BookSerializer
+
+
+
+from rest_framework import viewsets
+from rest_framework.serializers import ModelSerializer
+# Replace 'my_app.models.Book' with your actual Book model path
+from api.models import Book
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for CRUD operations on Book model
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
