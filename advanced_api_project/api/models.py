bfe74b4f7c1c082aa3d9e7622203add9e941fb1b
Hagos Chekole <Hagochek06@gmail.com>
2024-09-08 00:47:12 +0300

    api is committed

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-  name=models.CharField()
+  name=models.CharField(max_length=100)
 class Book(models.Model):
-  title=models.CharField()
+  title=models.CharField(max_length=100)
   publication_year=models.IntegerField()
   author=models.ForeignKey(Author)
