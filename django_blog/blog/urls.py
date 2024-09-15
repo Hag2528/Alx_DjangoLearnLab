35edad71846ddaec05b925edadc5e19bc0b0fec3
Hagos Chekole <Hagochek06@gmail.com>
2024-09-15 19:44:22 +0300

    committed

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('new/', views.PostCreateView.as_view(), name='post_create'),
+    path('new/', views.PostCreateView.as_view(), name='post_form'),
 
     path('<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
     path('<int:pk>/delete/', views.PostDeleteView.as_view(),name='post_delete'),
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -30,18 +30,18 @@ from .models import  Post
 from .forms import PostForm 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/post_list.html'
+    template_name = 'blog/templates/post_list.html'
     context_object_name = 'posts'
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'blog/post_detail.html'
+    template_name = 'blog/templates/post_detail.html'
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'blog/post_form.html'
+    template_name = 'blog/templates/post_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -51,7 +51,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
-    template_name = 'blog/post_form.html'
+    template_name = 'blog/templates/post_edit.html'
 
     def test_func(self):
         post = self.get_object()
@@ -59,7 +59,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name = 'blog/post_confirm_delete.html'
+    template_name = 'blog/templates/post_confirm_delete.html'
 
     success_url = '/posts/'
 
