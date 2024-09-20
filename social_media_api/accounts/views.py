233db14bc416d0c833a8765d0ccd518c18a3fc36
Hagos Chekole <Hagochek06@gmail.com>
2024-09-20 15:29:31 +0300

    committed

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, permissions
+from .models import User
+from rest_framework.authtoken.views import ObtainAuthToken
+from .serializers import UserSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
