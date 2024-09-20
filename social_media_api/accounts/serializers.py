4ce9d71878e1ebaad3d1835b2f2ca713dc1fc92a
Hagos Chekole <Hagochek06@gmail.com>
2024-09-20 15:28:00 +0300

    committed

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,9 +1,15 @@
 
 from rest_framework import serializers
 from .models import User
-
+from rest_framework.authtoken.models import Token
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username','email', 'password', 'bio', 'profile_picture', 'followers')
         extra_kwargs = {'password': {'write_only': True}}
+
+
+        serializers.CharField() 
+        Token.objects.create, 
+        def  get_user_model():
+          User.objects.create_user
