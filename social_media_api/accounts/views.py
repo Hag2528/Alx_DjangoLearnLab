a0b6ac7a72b266977bba333c96f92c7166d19099
Hagos Chekole <Hagochek06@gmail.com>
2024-09-20 17:57:48 +0300

    social media is committed

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,14 +1,53 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
-from rest_framework import generics, permissions
-from .models import User
-from rest_framework.authtoken.views import ObtainAuthToken
+# # Create your views here.
+# from rest_framework import generics, permissions
+# from .models import User
+# from rest_framework.authtoken.views import ObtainAuthToken
+# from .serializers import UserSerializer
+
+# class RegisterView(generics.CreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+# class LoginView(ObtainAuthToken):
+#     pass
+
+
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from django.contrib.auth import authenticate, login
 from .serializers import UserSerializer
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
 
-class LoginView(ObtainAuthToken):
-    pass
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if username is None or password is None:
+            return Response({'error': 'Please provide both username and password'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        login(request, user)
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}) 
+
+
+class ProfileView(APIView):
+    def get(self, request):
+        user = request.user  # Access authenticated user from request
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
